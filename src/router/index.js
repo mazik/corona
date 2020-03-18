934c21f073da40f336cebe2c9be1a51d6ac67dc8
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2020-03-18 18:00:14 +0600

    Settings page

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,13 @@ const routes = [
     component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue")
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/settings",
+    name: "Settings",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (settings.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "settings" */ "../views/Settings.vue")
   }
 ];
 
